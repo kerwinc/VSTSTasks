6a1377d8549d6e1128fc154c0ab3dce67d5d11ac
Unknown <kerwinc@videojunky.co.za>
2017-09-24 21:21:38 +0200

    Added gitignore and PS to package extensions

--- Tasks/GitflowBranchGate/Package.ps1
+++ Tasks/GitflowBranchGate/Package.ps1
@@ -8,13 +8,13 @@ $taskJson.version.Patch = "$([System.Convert]::ToInt32($taskJson.version.Patch) 
 
 [string]$newVersionNumber = "$($taskJson.version.Major).$($taskJson.version.Minor).$($taskJson.version.Patch)"
 Write-Host "New Version: $newVersionNumber"
-$taskJson | ConvertTo-Json | set-content ".\Task\task.json"
+$taskJson | ConvertTo-Json -Compress | set-content ".\Task\task.json"
 
 $extensionJson = Get-Content -Path "vss-extension.json" -raw | ConvertFrom-Json
 Write-Host "Updating VSS-Extension: $($extensionJson.name)" -ForegroundColor Yellow
 Write-Host "Current Version: $($taskJson.version)"
 Write-Host "New Version: $newVersionNumber"
 $extensionJson.version = "$newVersionNumber"
-$extensionJson | ConvertTo-Json | set-content "vss-extension.json"
+$extensionJson | ConvertTo-Json -Compress | set-content "vss-extension.json"
 
 tfx extension create --manifest vss-extension.json --output-path .\build\
